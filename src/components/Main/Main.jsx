34e876dee1af3d608bf29ce381c525255d48952e
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-12 17:08:11 +0300

    img added

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import './Main.scss';
+import {data} from '../../helper/data';
+
+const Main = () => {
+    return (
+        <div className="card-container">
+        {data.map((item) =>{
+            return(
+                <div className="cards">
+                <h1>{item.title}</h1>
+                <img src={item.image} alt="" />
+                </div>
+            )
+
+        })}
+        </div>
+    )
+}
+
+export default Main;
